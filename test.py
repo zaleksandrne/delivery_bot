0807ca44a2da2f18a265dc236469c8489d58e5c4
alex <zaleksandrne@gmail.com>
2021-04-04 19:17:19 +0300

    .

--- test.py
+++
@@ -1,9 +0,0 @@
-import json
-with open('shiptor_cities.json', 'r', encoding='utf-8') as g:
-    shiptor_cities = json.load(g)
-derival_shiptor = [city['kladr_id'] for city in shiptor_cities if city.get('name', '').lower() == ('Луховицы').lower()] 
-#for item in GLAV_CITIES:
- #   del item['parents']
-#with open('a.json', 'w', encoding='utf-8') as f:
- #   json.dump(GLAV_CITIES, f, indent=4, ensure_ascii=False,)
-print(derival_shiptor[0])
